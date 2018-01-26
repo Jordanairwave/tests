1acb83f1c45b1a78df439cf852fe8fbded4ae403
Jordan Haines <jordanhaines@Jordans-iMac.local>
2018-01-26 17:20:51 +0000

    head reset - upload error

--- interactive-map/scripts.js
+++ interactive-map/scripts.js
@@ -1,6 +1,22 @@
 var site = {
 	init: function() {
 
+        //Map mouse enter functionality
+        $('#svg2 .region').on('mouseenter', function() {
+
+            //Add class hover to current region
+            $(this).attr("class", "region hover");
+        });
+
+        //Map mouse leave functionality
+        $('#svg2 .region').on('mouseleave', function() {
+
+            //remove class hover to current region
+            $(this).attr("class", "region");
+        });
+
+
+
     }
 };
 
