f4690ed0e0931e7fd9a1781091250f65099385b1
Jordan Haines <jordanhaines@Jordans-iMac.local>
2018-02-05 10:02:34 +0000

    PDO script added and working

--- interactive-map/find-sales-guy.php
+++ interactive-map/find-sales-guy.php
@@ -3,40 +3,59 @@
     PHP script to find sales guy information when region is click
 */
 
-//Database Details
-$host = '127.0.0.1';
-$dbname = 'interactive-map';
-$user = 'root';
-$pass = 'root'
+if( !empty($_POST)) {
+    //Database Details - These would go in a seperate file
+    $host = '127.0.0.1';
+    $dbname = 'interactive-map';
+    $user = 'root';
+    $pass = 'root';
 
 
-//PDO Try statement
-try {   
-    # MySQL with PDO_MYSQL
-    $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    //PDO Try statement
+    try {   
+        # MySQL with PDO_MYSQL
+        $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-    $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-    //Prepared Statement
-    $STH = $DBH->query("SELECT * FROM sales WHERE region (:name)");
+        //SQL to use
+        $sql = "SELECT * FROM sales WHERE region = :name";
 
-    $name = "Scotland";
+        //Prepared Statement
+        $STH = $DBH->prepare($sql);
 
-    $STH->bindParam(':name', $name);
+        //Name of clicked Region
+        $name = $_POST['region'];
 
-    //set PDO::FETCH_ASSOC
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
+        //Bind name to name in SQL
+        $STH->bindParam(':name', $name, PDO::PARAM_STR); 
 
-    $STH->execute();
-   
+        //set PDO::FETCH_ASSOC
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+
+        //Execute Query
+        $STH->execute();
+
+        //Out Put Results
+        while($row = $STH->fetch()) {
+            echo $row['first_name'] . "\n";
+            echo $row['last_name'] . "\n";
+            echo $row['mobile_number'] . "\n";
+            echo $row['region'] . "\n";
+            echo $row['email'] . "\n";
+            echo $row['profile_pic'] . "\n";
+        }
+
+        # close the connection
+        $DBH = null;
+    
+    }
+
+    //PDO catch statement
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+    }
 }
-
-//PDO catch statement
-catch(PDOException $e) {
-    echo $e->getMessage();
-    file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
-}
-
-
 
 ?>
